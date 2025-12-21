e67ffe400872aeafaed32e16e7473ae3f3d0736e
Maithili Naik <maithilinaik0910@gmail.com>
2025-12-21 20:01:27 +0530

    Update ALLOWED_HOSTS for production environment

--- backend/flairies_backend/settings.py
+++ backend/flairies_backend/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost,192.168.0.7')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost,*.railway.app')
+
 
 
 # Application definition
@@ -134,3 +135,4 @@ CORS_ALLOW_HEADERS = [
 # Razorpay Configuration
 RAZORPAY_KEY_ID = config('RAZORPAY_KEY_ID', default='')
 RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET', default='')
+
